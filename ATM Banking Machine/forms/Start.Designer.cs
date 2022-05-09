fbcd36f668291bec1f1863783341d908d56b3373
Kuba <jakubkracla@gmail.com>
2022-05-09 14:33:07 +0200

    Added loading screen while opening the app

---
+++ ATM Banking Machine/forms/Start.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ATM_Banking_Machine
+{
+    partial class Start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Start));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.percentageLbl = new System.Windows.Forms.Label();
+            this.progressBarStart = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Text", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(41, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(726, 69);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ATM MANAGEMENT SYSTEM";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(285, 95);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(217, 194);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // percentageLbl
+            // 
+            this.percentageLbl.AutoSize = true;
+            this.percentageLbl.Font = new System.Drawing.Font("Sitka Text", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.percentageLbl.Location = new System.Drawing.Point(363, 304);
+            this.percentageLbl.Name = "percentageLbl";
+            this.percentageLbl.Size = new System.Drawing.Size(81, 69);
+            this.percentageLbl.TabIndex = 4;
+            this.percentageLbl.Text = "%";
+            this.percentageLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBarStart
+            // 
+            this.progressBarStart.Location = new System.Drawing.Point(0, 377);
+            this.progressBarStart.Name = "progressBarStart";
+            this.progressBarStart.Size = new System.Drawing.Size(799, 23);
+            this.progressBarStart.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(800, 400);
+            this.Controls.Add(this.progressBarStart);
+            this.Controls.Add(this.percentageLbl);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Start";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Start";
+            this.Load += new System.EventHandler(this.Start_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label percentageLbl;
+        private ProgressBar progressBarStart;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ ATM Banking Machine/forms/Start.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATM_Banking_Machine
+{
+    public partial class Start : Form
+    {
+        public Start()
+        {
+            InitializeComponent();
+        }
+        int starting = 0;
+        
+        private void Start_Load(object sender, EventArgs e) //starting the timer
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //progress bar loading
+        {
+            starting += 4;
+            progressBarStart.Value = starting;
+            percentageLbl.Text = "" + starting;
+            if(progressBarStart.Value == 100)
+            {
+                progressBarStart.Value = 0;
+                timer1.Stop();
+                Login log = new Login();
+                this.Hide();
+                log.Show();
+            }
+        }
+    }
+}
