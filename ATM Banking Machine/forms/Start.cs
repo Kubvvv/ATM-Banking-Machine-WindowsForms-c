a974849778209d966b9abd69d5453abe3e85b1a0
Kuba <jakubkracla@gmail.com>
2022-05-13 20:02:00 +0200

    Added a method that creates the database if it is not exist, and creates a sample user.

--- ATM Banking Machine/forms/Start.cs
+++ ATM Banking Machine/forms/Start.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ATM_Banking_Machine.Data;
+using ATM_Banking_Machine.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -21,6 +23,33 @@ namespace ATM_Banking_Machine
         private void Start_Load(object sender, EventArgs e) //starting the timer
         {
             timer1.Start();
+            using ATMDb context = new ATMDb();
+            context.Database.EnsureCreated();
+            createFirstUser();
+        }
+
+        private void createFirstUser() //creates first user if no exist
+        {
+            using ATMDb context = new ATMDb();
+            var isCreated = context.Accounts.SingleOrDefault(x => x.AccNum == "1"); // checking if there is an account with the same accnumber
+            if(isCreated == null)
+            {
+                Account acc = new Account()
+                {
+                    AccNum = "1",
+                    Name = "Jan",
+                    Surname = "Kowalski",
+                    DateOfBirth = "11.7.1957",
+                    Phone = "987123456",
+                    Address = "Warszawa",
+                    PIN = 1234,
+                    Education = "Post-Graduate",
+                    Occupation = "Programmer",
+                    Balance = 10000,
+                };
+                context.Add(acc);
+                context.SaveChanges();
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e) //progress bar loading
